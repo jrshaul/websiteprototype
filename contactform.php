b8702f25da7130fe1d9c1b9f6a1ceb26a5fe99cc
jrshaul <jrshaul@gmail.com>
2024-03-24 18:01:00 -0500

    changes

---
+++ contactform.php
@@ -0,0 +1,33 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    $to = "alyyashar@gmail.com";
+    $subject = "New email from your site!";
+    $fname = $_POST['fname'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+    
+    $headers .= "MIME-Version: 1.0\r\n";
+    $headers .= "Content-Type: text/html; charset=UTF-8\r\n";
+    
+    $title = '<h3>Hello! You have received a new mail from your website!</h3>';
+    
+    $body = "$title
+            <br/>
+            <b>From:</b> $fname
+            <br/>
+            <b>E-Mail:</b> $email
+            <br/>
+            <b>Message:</b>\n$message
+            <br/>
+            <br/>";
+
+    if (mail($to, $subject, $body, $headers)){
+        echo "<h1>Sent Successfully! Thank you"." ".$fname.", We will contact you shortly!</h1>";
+    } else { 
+        echo "Something went wrong!";
+    }
+}
+?>
+<br>
+<a href="/">Back to Homepage</a>
