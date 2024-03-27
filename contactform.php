83aac32f49299feeabf424aeb5a6b3a68534d561
jrshaul <jrshaul@gmail.com>
2024-03-27 15:43:40 -0500

    fix

--- contactform.php
+++ contactform.php
@@ -1,14 +1,22 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    
+    $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
 
-    $to = "jrshaul@gmail.com";
-    $subject = "New email from your site!";
+    if (!preg_match($email_exp, $email)) {
+        $error_message .= 'The Email address you entered does not appear to be valid.<br>';
+    }
+
+    $to = "jraphael@jraphael.photography";
+    $subject = "New email from";
     $fname = $_POST['fname'];
     $email = $_POST['email'];
     $message = $_POST['message'];
     
     $headers .= "MIME-Version: 1.0\r\n";
     $headers .= "Content-Type: text/html; charset=UTF-8\r\n";
+    
+    
     
     $title = '<h3>Hello! You have received a new mail from your website!</h3>';
     
